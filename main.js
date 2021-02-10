c6eb6f217f2185379f7f4470e8453dbe700f86d4
bongivilakazi <57225184+bongivilakazi@users.noreply.github.com>
2021-02-10 06:12:24 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,82 @@
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+const showMenu = (toggleId, navId) => {
+  const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId);
+
+  if (toggle && nav) {
+    toggle.addEventListener("click", () => {
+      nav.classList.toggle("show");
+    });
+  }
+};
+
+showMenu('nav-toggle','nav-menu')
+
+/*remove menu mobile**/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+  const navMenu = document.getElementById('nav-menu')
+  navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click',linkAction))
+
+/*scroll sections active links*/
+const sections = document.querySelectorAll('section[id]')
+window.addEventListener('scroll' ,scrollActive)
+
+function scrollActive(){
+  const scrollY = window.pageYOffset
+  sections.forEach(current =>{
+    const sectionHeight = current.offsetHeight
+    const sectionTop = current.offsetTop - 50;
+    sectionId = current.getAttribute('Id');
+    if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+      document.querySelector('.nav__menu a[href * ='+ sectionId + ']').classList.add('active')
+    }else{
+      document.querySelector('.nav__menu a[href* ='+sectionId + ']').classList.remove('active')
+  }
+
+})
+}
+
+/*scroll reveal animations*/
+const sr = scrollReveal({
+  origin:'top',
+  distance:'80px',
+  duration:2000,
+  reset:true
+  
+  })
+  
+  sr.scrollReveal('.home__title',{}) 
+  sr.scrollReveal('.home__scroll',{delay:200})
+  sr.scrollReveal('.home__img',{origin:'right',delay:400})
+     
+     
+      sr.reveal('.about__img',{delay:500})
+      sr.reveal('.about__subtitle',{delay:300})
+      sr.reveal('.about__profession',{delay:400})
+      sr.reveal('.about__text',{delay:500})
+      sr.reveal('.about__social-icon',{delay:600,interval:200})
+    
+      sr.reveal('.skills__subtitle',{})
+      sr.reveal('.skills__name',{distance:'20px',delay:50,interval:100})
+      sr.reveal('.skills__img',{delay:400})
+      
+      sr.reveal('.portfolio__img',{interval:400})
+      
+      sr.reveal('.contact__subtitles',{})
+      sr.reveal('.contact__text',{interval:200})
+      sr.reveal('.contact__input',{delay:400})
+      sr.reveal('.contact__text',{interval:200})
+      sr.reveal('.contact__button',{delay:600})
+/*scroll home*/
+/*scroll about*/
+
+
